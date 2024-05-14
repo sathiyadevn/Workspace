f83597b19b8d343bb09aa67f17d54cc6c2f6c6ba
sathiyadevn <sathiyadevnatarajan43@gmail.com>
2024-05-14 21:10:25 +0530

    configure JdbcTemplate -> database connectivity

--- springWorkspace/JDBCdemo/src/main/java/com/dev43/spring/jdbcdemo/repo/UserRepo.java
+++ springWorkspace/JDBCdemo/src/main/java/com/dev43/spring/jdbcdemo/repo/UserRepo.java
@@ -1,6 +1,8 @@
 package com.dev43.spring.jdbcdemo.repo;
 
 import com.dev43.spring.jdbcdemo.mdoel.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,6 +10,17 @@ import java.util.List;
 
 @Repository
 public class UserRepo {
+    private JdbcTemplate jdbcTemplate;      // Get database connectivity
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void save(User user){
         System.out.println("Added");
     }
