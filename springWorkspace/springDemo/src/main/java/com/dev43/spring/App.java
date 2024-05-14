af142f60582e560f9be897f0404547ad67e01d6b
sathiyadevn <sathiyadevnatarajan43@gmail.com>
2024-05-14 18:54:06 +0530

    Singleton -> Create an object by default, Prototype -> Won't

--- springWorkspace/springDemo/src/main/java/com/dev43/spring/App.java
+++ springWorkspace/springDemo/src/main/java/com/dev43/spring/App.java
@@ -10,17 +10,17 @@ public class App
 //        System.out.println( "Hello World!" );
 
         ApplicationContext factory=new ClassPathXmlApplicationContext("spring.xml");
-        User user= (User) factory.getBean("user");
-        user.code();
-        user.age=20;
-        System.out.println(user.age);       // 20
-
-        // Singleton -> Spring Container gives you the same obj for all .getbean() call
-        User user1= (User) factory.getBean("user");
-        System.out.println(user1.age);      // 20  -> 0
-
-        // Prototype
-        User user2= (User) factory.getBean("user");
-        System.out.println(user2.age);     // 0
+//        User user= (User) factory.getBean("user");
+//        user.code();
+//        user.age=20;
+//        System.out.println(user.age);       // 20
+//
+//        // Singleton -> Spring Container gives you the same obj for all .getbean() call
+//        User user1= (User) factory.getBean("user");
+//        System.out.println(user1.age);      // 20  -> 0
+//
+//        // Prototype
+//        User user2= (User) factory.getBean("user");
+//        System.out.println(user2.age);     // 0
     }
 }
--- springWorkspace/springDemo/src/main/java/com/dev43/spring/User.java
+++ springWorkspace/springDemo/src/main/java/com/dev43/spring/User.java
@@ -2,6 +2,11 @@ package com.dev43.spring;
 
 public class User {
     int age;
+
+    public User() {
+        System.out.println("Im User");
+    }
+
     public void code(){
         System.out.println("Coding...");
     }
