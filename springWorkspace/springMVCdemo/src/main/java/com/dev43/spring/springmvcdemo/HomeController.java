8dfa4138bc662f1c9622c9c20c507f6d79f4435c
sathiyadevn <sathiyadevnatarajan43@gmail.com>
2024-05-15 19:45:38 +0530

    Change to -> @GetMapping @PostMapping

--- springWorkspace/springMVCdemo/src/main/java/com/dev43/spring/springmvcdemo/HomeController.java
+++ springWorkspace/springMVCdemo/src/main/java/com/dev43/spring/springmvcdemo/HomeController.java
@@ -3,42 +3,40 @@ package com.dev43.spring.springmvcdemo;
 import com.dev43.spring.springmvcdemo.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @ModelAttribute
-    public void modelData(Model m){
-        m.addAttribute("name","Developer");
+    public void modelData(Model m) {
+        m.addAttribute("name", "Developer");
     }
 
     @RequestMapping("/")
-    public String home(){
+    public String home() {
+
         return "index";
     }
-    @RequestMapping("add")
-    public String add(@RequestParam("num1") int i, @RequestParam("num2")int j, Model m){
-
-        int z=i+j;
-        m.addAttribute("addValue",z);
-        return "result";
+    @GetMapping("getUsers")
+    public String getUser(Model m){
+        List<User> users= Arrays.asList(new User(101,"java"),new User(102,"python"));
+        m.addAttribute("getUsers",users);
+        return "showUsers";
     }
 
     @PostMapping("addUser")
-    public String addUser(@ModelAttribute("addUser") User user){
-//        User user=new User();
-//        user.setId(id);
-//        user.setName(name);
+    public String addUser(@ModelAttribute("addUser") User user) {
 
-//        m.addAttribute("addUser",user);
         return "result";
     }
-
 
 
 }
--- springWorkspace/springMVCdemo/src/main/webapp/views/index.jsp
+++ springWorkspace/springMVCdemo/src/main/webapp/views/index.jsp
@@ -8,17 +8,13 @@
 <body>
     <h1>Hai! Dev43</h1>
 
-<%--    <form action="add">--%>
-<%--        Enter 1st Number <input type="text" name="num1"> <br>--%>
-<%--        Enter 2nd Number <input type="text" name="num2"> <br>--%>
-<%--        <input type="submit">--%>
-<%--    </form>--%>
-
     <form action="addUser" method="post">
         Enter Id <input type="text" name="id">
         Enter Name <input type="text" name="name">
         <input type="submit">
     </form>
+
+    
 
 </body>
 </html>
