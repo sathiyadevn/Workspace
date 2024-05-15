1f338a78b7b1f8e02957d395113e633441fa701c
sathiyadevn <sathiyadevnatarajan43@gmail.com>
2024-05-15 14:36:52 +0530

    Projrct -> Maven + spring configuration, Not a Spring Boot Project, Issue - result.jsp not displaying output from index.jsp input

---
+++ springWorkspace/mavenSpringMVCdemo/src/main/java/com/dev43/spring/springmvcdemo/HomeController.java
@@ -0,0 +1,42 @@
+package com.dev43.spring.springmvcdemo;
+
+import com.dev43.spring.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @ModelAttribute
+    public void modelData(Model m){
+        m.addAttribute("name","Developer");
+    }
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int i, @RequestParam("num2")int j, Model m){
+
+        int z=i+j;
+        m.addAttribute("addValue",z);
+        return "result";
+    }
+
+    @RequestMapping("addUser")
+    public String addUser(@ModelAttribute("addUser") User user){
+//        User user=new User();
+//        user.setId(id);
+//        user.setName(name);
+
+//        m.addAttribute("addUser",user);
+        return "result";
+    }
+
+
+
+}
