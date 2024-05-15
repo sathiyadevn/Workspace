8f0e883d4a95b4558164e6ef69b20d2feae64312
sathiyadevn <sathiyadevnatarajan43@gmail.com>
2024-05-15 11:10:34 +0530

    secure folder - webapp - In Application.properties -> spring.mvc.view.prefix & suffix

--- springWorkspace/springMVCdemo/src/main/java/com/dev43/spring/springmvcdemo/HomeController.java
+++ springWorkspace/springMVCdemo/src/main/java/com/dev43/spring/springmvcdemo/HomeController.java
@@ -1,26 +1,24 @@
 package com.dev43.spring.springmvcdemo;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String home(){
-        return "index.jsp";
+        return "index";
     }
     @RequestMapping("add")
-    public String add(HttpServletRequest req){
-        int i=Integer.parseInt(req.getParameter("num1"));
-        int j=Integer.parseInt(req.getParameter("num2"));
+    public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2")int j){
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("result");
+
         int z=i+j;
-
-        HttpSession session= req.getSession();
-        session.setAttribute("addValue",z);
-
-        return "result.jsp";
+        mv.addObject("addValue",z);
+        return mv;
     }
 
 }
