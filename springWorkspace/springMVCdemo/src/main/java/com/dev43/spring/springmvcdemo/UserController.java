11f1eea1bdc9b2c80b9b7cdafff3c213cfecb00d
sathiyadevn <sathiyadevnatarajan43@gmail.com>
2024-05-16 10:50:38 +0530

    @RestController - No Need @ResponseBody for each methods & @PathVariable()

--- springWorkspace/springMVCdemo/src/main/java/com/dev43/spring/springmvcdemo/UserController.java
+++ springWorkspace/springMVCdemo/src/main/java/com/dev43/spring/springmvcdemo/UserController.java
@@ -2,22 +2,27 @@ package com.dev43.spring.springmvcdemo;
 
 import com.dev43.spring.springmvcdemo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
     @Autowired
     UserRepo repo;
 
     @GetMapping("users")
-    @ResponseBody
     public List<User> getUsers(Model m){
         List<User> users = repo.findAll();
     return users;
+    }
+
+    @GetMapping("users/{userId}")
+    public User getUser(@PathVariable("userId") int id){
+        User user=repo.findById(id).orElse(new User(0,""));
+        return user;
     }
 }
