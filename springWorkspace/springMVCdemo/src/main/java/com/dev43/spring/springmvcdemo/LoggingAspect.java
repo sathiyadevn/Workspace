a0ce275e076c141cfd5d67ba2ed3cd8deec067ad
sathiyadevn <sathiyadevnatarajan43@gmail.com>
2024-05-17 12:16:05 +0530

    Working with Logger -> aap.log

--- springWorkspace/springMVCdemo/src/main/java/com/dev43/spring/springmvcdemo/LoggingAspect.java
+++ springWorkspace/springMVCdemo/src/main/java/com/dev43/spring/springmvcdemo/LoggingAspect.java
@@ -2,14 +2,18 @@ package com.dev43.spring.springmvcdemo;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
+    private static final Logger LOGGER=LoggerFactory.getLogger(LoggingAspect.class);
     @Before("execution(public * com.dev43.spring.springmvcdemo.HomeController.getUsers())")
     public void log(){
-        System.out.println("getUsers() method called from Aspect");
+
+        LOGGER.info("getUsers() method called from Aspect");
     }
 }
